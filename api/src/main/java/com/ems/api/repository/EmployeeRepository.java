ab44171e6febd5d182fab7a771f3e6291d8e8f25
kayode adechinan <kayode.adechinan@gmail.com>
2019-10-21 16:48:34 -0400

    initial

---
+++ api/src/main/java/com/ems/api/repository/EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.ems.api.repository;
+
+import com.ems.api.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Long>{
+
+}
